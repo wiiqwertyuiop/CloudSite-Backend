f6f0b2a6320ef7b2d8c932cf208125fb78155905
bob sagat <wiiqwertyuiop@gmail.com>
2023-10-26 16:23:56 -0400

    code change

--- functions/main.py
+++ functions/main.py
@@ -1,14 +1,17 @@
+import random
 from firebase_functions import https_fn, scheduler_fn
 from firebase_admin import initialize_app, firestore
 import google_crc32c
+from english_words import get_english_words_set
 
 initialize_app()
 
 # Run once a day to get a new word
 @scheduler_fn.on_schedule(schedule="every day 04:00", region="us-east1")
 def daily_word(event: scheduler_fn.ScheduledEvent) -> None:
+    words = get_english_words_set(['web2'])
     firestore_client: google_crc32c.cloud.firestore.Client = firestore.client()
-    firestore_client.collection("messages").document("1").set({"word": "cat"})
+    firestore_client.collection("messages").document("1").set({"word": words.pop()})
 
 # Retrieve word
 @https_fn.on_request(region="us-east1")
